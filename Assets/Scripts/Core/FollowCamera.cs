2d9f5e47878242c29bd4baec0fb1eb683e8e4caf
Will <wcorrigan90@gmail.com>
2020-11-19 06:25:17 +0000

    lesson 30 finished

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+namespace RPG.Core
+{ 
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        private void LateUpdate()
+        {
+            transform.position = target.position;
+        }
+    }
+}
+
